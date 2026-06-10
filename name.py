588a8b18e3f5e29539f81f00de68cec26969c1c7
taranumbhatia87-art <taranumbhatia87@gmail.com>
2026-06-10 20:32:48 +0530

    Add files via upload
    homework

---
+++ name.py
@@ -0,0 +1,17 @@
+age = int(input("what is your age?"))
+print("oh you are", age, "years old") 
+name = input("what is your name?")
+print("hello", name) 
+hobby = input("what is your hobby?") 
+print(" wow you like", hobby) 
+
+
+
+
+name = "dhwaj" 
+hobbies = ["coding", "chess", "football", "reading"] 
+favourite_foods = ["pasta","paneer", "icecream"]
+favourite_books = ["harry potter and the goblet of fire", "percy jackson and the titans curse"] 
+
+
+print("my hobbies are", hobbies)
